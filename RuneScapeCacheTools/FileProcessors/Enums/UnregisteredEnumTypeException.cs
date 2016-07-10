8a018e3d0e282728e21b2d4594a28b5d42b34087
Villermen <villermen@gmail.com>
2016-07-10 16:12:27 +0200

    Leftover EnumValueType -> EnumType

---
+++ RuneScapeCacheTools/FileProcessors/Enums/UnregisteredEnumTypeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Villermen.RuneScapeCacheTools.FileProcessors.Enums
+{
+	public class UnregisteredEnumTypeException : Exception
+	{
+		public UnregisteredEnumTypeException()
+		{
+		}
+
+		public UnregisteredEnumTypeException(string message) : base(message)
+		{
+		}
+
+		public UnregisteredEnumTypeException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
