3f630301d831e413af19d9b62fdd771504351927
Villermen <villermen@gmail.com>
2016-07-27 19:51:00 +0200

    Test ogg export script

--- RuneScapeCacheToolsCLI/Program.cs
+++ RuneScapeCacheToolsCLI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using Villermen.RuneScapeCacheTools.Cache;
 using Villermen.RuneScapeCacheTools.Cache.RuneTek5;
 using Villermen.RuneScapeCacheTools.Cache.RuneTek5.Audio;
@@ -21,21 +23,23 @@ namespace Villermen.RuneScapeCacheTools.CLI
 			//var sortingTrackNames = new EnumFile(cache.GetArchiveFileData(17, 5, 67));
 			var jagaFileIds = new EnumFile(cache.GetArchiveFileData(17, 5, 71));
 
-			var d = jagaFileIds[5];
-
-			foreach (var soundtrackFileId in jagaFileIds) // Add indexing and enumerating to EnumFile
+			foreach (var soundtrackFileIdPair in jagaFileIds)
 			{
 				try
 				{
-					var soundtrackFileData = cache.GetFileData(40, soundtrackFileId);
+					var soundtrackFileData = cache.GetFileData(40, (int) soundtrackFileIdPair.Value);
 					var jagaFile = new JagaFile(soundtrackFileData);
 
+					File.WriteAllBytes("0.ogg", jagaFile.ContainedChunkData);
 
+					for (var chunkIndex = 1; chunkIndex < jagaFile.ChunkCount; chunkIndex++)
+					{
+						File.WriteAllBytes($"{chunkIndex}.ogg", cache.GetFileData(40, jagaFile.ChunkDescriptors[chunkIndex].FileId));
+					}
 				}
 				catch (Exception ex) when (ex is JagaParseException)
 				{
 				}
-
 			}
 
 			// Console.ReadLine();
