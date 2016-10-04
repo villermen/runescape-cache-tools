5cceb153e6d776fc4894fa9ffb1685824bf324fb
Villermen <villermen@gmail.com>
2016-10-04 12:59:27 +0200

    Add tests for downloader caching
    Use get methods instead of download methods for retrieving the reference tables.

--- RuneScapeCacheToolsTests/DownloaderTests.cs
+++ RuneScapeCacheToolsTests/DownloaderTests.cs
@@ -40,12 +40,12 @@ namespace RuneScapeCacheToolsTests
         [Fact]
         public void TestDownloadReferenceTable()
         {
-            var referenceTable12 = Fixture.Downloader.DownloadReferenceTable(Index.ClientScripts);
-            var referenceTable40 = Fixture.Downloader.DownloadReferenceTable(Index.Music);
+            var referenceTable12 = Fixture.Downloader.GetReferenceTable(Index.ClientScripts);
+            var referenceTable40 = Fixture.Downloader.GetReferenceTable(Index.Music);
 
             var rawReferenceTable = Fixture.Downloader.GetFile(Index.ReferenceTables, (int)Index.Enums);
 
-            var referenceTable17 = Fixture.Downloader.DownloadReferenceTable(Index.Enums);
+            var referenceTable17 = Fixture.Downloader.GetReferenceTable(Index.Enums);
 
             Output.WriteLine($"Files in reference table for index 17: {referenceTable17.Files.Count}.");
 
@@ -55,7 +55,7 @@ namespace RuneScapeCacheToolsTests
         [Fact]
         public void TestDownloadMasterReferenceTable()
         {
-            var masterReferenceTable = Fixture.Downloader.DownloadMasterReferenceTable();
+            var masterReferenceTable = Fixture.Downloader.GetMasterReferenceTable();
 
             Assert.True(masterReferenceTable.ReferenceTableFiles.Count == 50, $"Master reference table reported having {masterReferenceTable.ReferenceTableFiles.Count} files instead of the expected 50.");
         }
@@ -82,6 +82,24 @@ namespace RuneScapeCacheToolsTests
         public void TestHttpInterface()
         {
             var httpFile = Fixture.Downloader.GetFile(Index.Music, 30498);
+        }
+
+        [Fact]
+        public void TestReferenceTableCaching()
+        {
+            var referenceTable1 = Fixture.Downloader.GetReferenceTable(Index.Enums);
+            var referenceTable2 = Fixture.Downloader.GetReferenceTable(Index.Enums);
+
+            Assert.Same(referenceTable1, referenceTable2);
+        }
+
+        [Fact]
+        public void TestMasterReferenceTableCaching()
+        {
+            var masterReferenceTable1 = Fixture.Downloader.GetMasterReferenceTable();
+            var masterReferenceTable2 = Fixture.Downloader.GetMasterReferenceTable();
+
+            Assert.Same(masterReferenceTable1, masterReferenceTable2);
         }
 
         public void Dispose()
