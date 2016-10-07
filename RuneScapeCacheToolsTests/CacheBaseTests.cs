5e87e1919b5af9173750cf4e1a9d5d002863669d
Villermen <villermen@gmail.com>
2016-10-07 22:19:38 +0200

    Move most RuneTek5CacheTests into new CacheBaseTests class

--- RuneScapeCacheToolsTests/CacheBaseTests.cs
+++ RuneScapeCacheToolsTests/CacheBaseTests.cs
@@ -28,7 +28,7 @@ namespace RuneScapeCacheToolsTests
 
             var startTime = DateTime.UtcNow - TimeSpan.FromSeconds(1);
 
-            Fixture.RuneTek5Cache.Extract(index, fileId, true);
+            Fixture.Cache.Extract(index, fileId, true);
 
             Assert.True(File.Exists(expectedFilePath), $"File was not extracted, or not extracted to {expectedFilePath}.");
 
@@ -56,11 +56,41 @@ namespace RuneScapeCacheToolsTests
         [Fact]
         public void TestExtractExtension()
         {
+            // TODO: Use pre-built cache for this
             Fixture.Downloader.Extract(Index.LoadingSprites, 8501);
 
             // Verify that the .jpg extension was added
-
             Assert.True(File.Exists($"output/extracted/{Index.LoadingSprites}/8501.jpg"));
+        }
+
+        /// <summary>
+        /// Test for a file that exists, an archive file that exists and a file that doesn't exist.
+        /// </summary>
+        [Fact]
+        public void TestGetFile()
+        {
+            var file = Fixture.Cache.GetFile(Index.ClientScripts, 3);
+
+            var fileData = file.Data;
+
+            Assert.True(fileData.Length > 0, "File's data is empty.");
+
+            var archiveFile = Fixture.Cache.GetFile(Index.Enums, 5);
+
+            var archiveEntry = archiveFile.Entries[255];
+
+            Assert.True(archiveEntry.Length > 0, "Archive entry's data is empty.");
+
+            try
+            {
+                Fixture.Cache.GetFile(Index.Music, 30);
+
+                Assert.True(false, "Cache returned a file that shouldn't exist.");
+            }
+            catch (CacheException exception)
+            {
+                Assert.True(exception.Message.Contains("incomplete"), "Non-existent file cache exception had the wrong message.");
+            }
         }
     }
 }
--- RuneScapeCacheToolsTests/RuneTek5CacheTests.cs
+++ RuneScapeCacheToolsTests/RuneTek5CacheTests.cs
@@ -22,36 +22,6 @@ namespace RuneScapeCacheToolsTests
             Fixture = fixture;
         }
 
-        /// <summary>
-        /// Test for a file that exists, an archive file that exists and a file that doesn't exist.
-        /// </summary>
-        [Fact]
-        public void TestGetFile()
-        {
-            var file = Fixture.RuneTek5Cache.GetFile(Index.ClientScripts, 3);
-
-            var fileData = file.Data;
-
-            Assert.True(fileData.Length > 0, "File's data is empty.");
-
-            var archiveFile = Fixture.RuneTek5Cache.GetFile(Index.Enums, 5);
-
-            var archiveEntry = archiveFile.Entries[255];
-
-            Assert.True(archiveEntry.Length > 0, "Archive entry's data is empty.");
-
-            try
-            {
-                Fixture.RuneTek5Cache.GetFile(Index.Music, 30);
-
-                Assert.True(false, "Cache returned a file that shouldn't exist.");
-            }
-            catch (CacheException exception)
-            {
-                Assert.True(exception.Message.Contains("incomplete"), "Non-existent file cache exception had the wrong message.");
-            }
-        }
-
         [Theory]
         [InlineData(Index.Music)]
         [InlineData(Index.Enums)]
