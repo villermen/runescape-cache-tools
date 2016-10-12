b7040eb4e2dbcec157320759451cbafe8ba76c31
Villermen <villermen@gmail.com>
2016-10-12 08:58:08 +0200

    Remove space between -c and comment string with oggCat
    Might fix oggCat on linux

--- RuneScapeCacheTools/Audio/Soundtrack.cs
+++ RuneScapeCacheTools/Audio/Soundtrack.cs
@@ -110,7 +110,7 @@ namespace Villermen.RuneScapeCacheTools.Audio
                             UseShellExecute = false,
                             CreateNoWindow = true,
                             Arguments =
-                                $"-c \"EXTRACTED_BY=Viller's RuneScape Cache Tools;VERSION={jagaFileInfo.Version}\" " +
+                                $"-c\"EXTRACTED_BY=Villers RuneScape Cache Tools;VERSION={jagaFileInfo.Version}\" " +
                                 $"\"{outputPath}\" " +
                                 "\"" + string.Join("\" \"", randomTemporaryFilenames) + "\""
                         }
