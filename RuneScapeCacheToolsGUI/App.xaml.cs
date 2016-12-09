0f019e0d5cc49e69929dd3aea7b9f23fb336a23d
Villermen <villermen@gmail.com>
2016-12-09 09:57:15 +0100

    Remove GUI directory

--- RuneScapeCacheToolsGUI/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace RuneScapeCacheToolsGUI
-{
-	/// <summary>
-	///     Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application { }
-}
