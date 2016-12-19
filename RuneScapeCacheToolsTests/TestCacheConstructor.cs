88c3f7120508e196f4abfbb3d6433c9c734f2e8a
Villermen <villermen@gmail.com>
2016-12-19 10:18:57 +0100

    Layout for creating a test cache

---
+++ RuneScapeCacheToolsTests/TestCacheConstructor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Villermen.RuneScapeCacheTools.Cache;
+using Villermen.RuneScapeCacheTools.Cache.CacheFile;
+using Villermen.RuneScapeCacheTools.Cache.Downloader;
+using Villermen.RuneScapeCacheTools.Cache.RuneTek5;
+using Xunit;
+
+namespace RuneScapeCacheToolsTests
+{
+    public class TestCacheConstructor
+    {
+        /// <summary>
+        /// Only used manually to construct a new test cache.
+        /// 
+        /// Downloads and stores files necessary to perform all tests.
+        /// </summary>
+        [Fact(Skip = "Only run manually to construct a new test cache")]
+        public void ConstructTestcache()
+        {
+            var files = new List<Tuple<Index, int>>
+            {
+                new Tuple<Index, int>(Index.Enums, 5),
+                new Tuple<Index, int>(Index.ClientScripts, 3),
+
+            };
+
+            var cache = new RuneTek5Cache("newCache", false);
+            var downloader = new CacheDownloader();
+
+            foreach (var fileTuple in files)
+            {
+                cache.PutFile(downloader.GetFile<DataCacheFile>(fileTuple.Item1, fileTuple.Item2));
+            }
+        }
+    }
+}
