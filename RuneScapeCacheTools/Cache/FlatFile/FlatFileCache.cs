4358ecd9d4d01d9de182cd7b7eed9b7845488844
Villermen <villermen@gmail.com>
2017-05-08 11:57:49 +0200

    Base for flat file cache

---
+++ RuneScapeCacheTools/Cache/FlatFile/FlatFileCache.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Villermen.RuneScapeCacheTools.Cache.FileTypes;
+
+namespace Villermen.RuneScapeCacheTools.Cache.FlatFile
+{
+    [Obsolete("Unfinished")]
+    public class FlatFileCache : CacheBase
+    {
+        public override IEnumerable<Index> Indexes
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override BinaryFile FetchFile(Index index, int fileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<int> GetFileIds(Index index)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PutFile(BinaryFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override CacheFileInfo GetFileInfo(Index index, int fileId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
