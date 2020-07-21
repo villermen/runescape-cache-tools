fdc53775d54fc3e978a485ed19f948b2ea603e98
Villermen <villermen@gmail.com>
2020-07-21 22:58:41 +0200

    Fix TestGetFileWithEntries()

--- RuneScapeCacheToolsTests/Tests/DownloaderCacheTests.cs
+++ RuneScapeCacheToolsTests/Tests/DownloaderCacheTests.cs
@@ -21,7 +21,7 @@ namespace Villermen.RuneScapeCacheTools.Tests.Tests
         {
             var archiveFile = this.Fixture.DownloaderCache.GetFile<EntryFile>(Index.Enums, 5);
 
-            Assert.Equal(249, archiveFile.EntryCount);
+            Assert.Equal(256, archiveFile.EntryCount);
         }
 
         [Fact]
